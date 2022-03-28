4dd7136036260a2d423be07f9c85a444c1570de2
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2022-03-29 11:43:48 +1300

    added code / enter a number

--- 01_lopping_basic.py
+++ 01_lopping_basic.py
@@ -1,6 +1,14 @@
 valid = False
 while not valid:
 
-    print("this is a loop")
-    # keep_going = input("again")
+    response = float(input("Enter a number: "))
 
+    if response > 0:
+      valid = True 
+
+    else: 
+        print("please enter a number that is more than zero")  
+        print()         
+
+
+        
